3ec168001704c1a683221b2e22f20e90aa8b389f
Abhijit Mondal <amondal@roku.com>
2025-09-03 15:10:08 +0530

    updating...

--- matrix-chain-multiplication/mytest.py
+++ matrix-chain-multiplication/mytest.py
@@ -59,17 +59,17 @@ print(torch.abs(d1-d2).max())
 
 assert torch.allclose(d1, d2, atol=1e-5), "Error in custom matrix impl CPU"
 
-# matrices_gpu = [x.to(device='cuda:0') for x in matrices]
+matrices_gpu = [x.to(device='cuda:0') for x in matrices]
 
-# start = time.time()*1000
-# d3 = extension_cpp.dot_chain_gpu(matrices_gpu)
-# end = time.time()*1000
-# print("Custom GPU Forward Pass Duration = ", end-start)
-# print("Custom GPU Forward Pass Output\n", d3)
-# print()
+start = time.time()*1000
+d3 = extension_cpp.dot_chain_gpu(matrices_gpu)
+end = time.time()*1000
+print("Custom GPU Forward Pass Duration = ", end-start)
+print("Custom GPU Forward Pass Output\n", d3)
+print()
 
-# d3 = d3/max_val
+d3 = d3/max_val
 
-# print(torch.abs(d1-d3).max())
+print(torch.abs(d1-d3).max())
 
-# assert torch.allclose(d1, d3, atol=1e-5), "Error in custom matrix impl GPU"
+assert torch.allclose(d1, d3, atol=1e-5), "Error in custom matrix impl GPU"
